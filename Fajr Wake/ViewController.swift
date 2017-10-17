31e0189a65aab19ba72185f7e9ef6039a34c5bb9
alimir1 <contact@alimir.io>
2017-10-17 13:14:51 -0700

    Initial Commit

---
+++ Fajr Wake/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Fajr Wake
+//
+//  Created by Ali Mir on 10/17/17.
+//  Copyright © 2017 com.AliMir. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
